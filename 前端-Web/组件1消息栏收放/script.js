d420940f8dd82c9cd8f74acafd5c39603d5026eb
game-idiot <zzj2032516667@gmail.com>
2025-04-09 17:15:06 +0800

    Delete 前端-Web directory

--- 前端-Web/组件1消息栏收放/script.js
+++
@@ -1,8 +0,0 @@
-const showHideBtn = document.querySelector(".show-hide-btn");
-const container = document.querySelector(".container");
-
-showHideBtn.addEventListener("click", () => {
-  document.startViewTransition(() => {
-    container.classList.toggle("expanded");
-  });
-});
