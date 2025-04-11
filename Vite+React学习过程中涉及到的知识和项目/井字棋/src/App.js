4ab35bb815f0eb2e381f875755fd12a2cfbae71b
张泽洁 <3296592663@qq.com>
2025-04-11 20:35:15 +0800

    提交项目
    井字棋创建一个可以交互的组件，点击value值为X

--- Vite+React学习过程中涉及到的知识和项目/井字棋/src/App.js
+++ Vite+React学习过程中涉及到的知识和项目/井字棋/src/App.js
@@ -1,27 +1,37 @@
+import { useState } from "react";
 export default function Board() {
   // React 组件必须返回单个 JSX 元素，不能像两个按钮那样返回多个相邻的 JSX 元素。
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
 // 构建可以重用的组件，React 的组件架构可以创建可重用的组件，以避免混乱、重复的代码
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+  // 点击按钮时，设置value为X
+  function handleClick() {
+    setValue("X");
+  }
   //传递value组件给Square组件
-  return <button className="square">{value}</button>;
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
